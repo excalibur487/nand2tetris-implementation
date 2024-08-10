5fb53fb179499d775fd333c8f52080317ac9c05b
Siddarth <siddarth2003s@gmail.com>
2024-08-10 15:45:05 -0400

    Implemented ALU

--- ALU/lol.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	for (int i = 0; i < 16; i++) {
-		printf("    FullAdder (a=a[%d], b=b[%d], c=a%dAddb%d, sum=out[%d], out=a%dAddb%d);\n", i, i, i-1, i-1, i, i, i);
-	}
-}

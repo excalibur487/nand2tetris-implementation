1b6a0e8426ab4fb935af3a366da85f50f4024d60
Siddarth <siddarth2003s@gmail.com>
2024-08-11 17:23:26 -0400

    Implmented RAM8 and added schematics

--- 03/a/hdlgen.c
+++ 03/a/hdlgen.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	for (int i = 0; i < 16; i++) {
-		printf("    Bit (in=in[%d], load=load, out=out[%d]);\n", i, i);
+	for (int i = 1; i < 9; i++) {
+		printf("    Register (in=in, load=r%d, out=o%d);\n", i, i);
 	}
 }
 
